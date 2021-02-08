55d6412b099829c4e31bdb545ffeaa0a2c034431
Callum <cdrob@hotmail.com>
2021-02-07 21:52:12 -0500

    cleaning up

--- Lab2/Ogre.cpp
+++ Lab2/Ogre.cpp
@@ -8,7 +8,6 @@ Ogre::Ogre(string name, int hvalue, Weapon &weapon)
 	setHealthValue(hvalue);
 	setWeapon(weapon);
 	convertName();
-	//convertAbility();
 }
 
 
@@ -17,20 +16,11 @@ void Ogre::convertName()
 	setName(getName() + " from the swamp");
 }
 
-void Ogre::convertAbility()
-{
-	string newAbility1 = "'Get out me swamp'" + getWeapon().getSpecialAbility(0);
-	getWeapon().setSpecialAbility(0, newAbility1);
-	getWeapon().setSpecialAbility(1, "'Donkey!'" + getWeapon().getSpecialAbility(1));
-}
 
 void Ogre::displayInfo()
 {
 	cout << "Name: " << getName() << "\nHealth: " << getHealthValue();
 	cout << getWeapon();		
 			
-		/* "\nWeapon: " << getWeapon().getName()
-	<< "\nWeapon Description: " << getWeapon().getDescription() << "\nWeapon Damage: " << getWeapon().getDamageValue()
-	<< "\nWeapon Abilities: 'Get out me swamp' " << getWeapon().getSpecialAbility(0) << ", 'Donkey!' "
-	<< getWeapon().getSpecialAbility(1 )<< endl << endl;*/
+	
 }
